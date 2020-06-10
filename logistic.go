c3c70b23b96b517223940f75161a0d9053960e25
passwind <mike.zhuyu@gmail.com>
2020-06-10 10:35:34 +0800

    Add Logistic init

--- logistic.go
+++ logistic.go
@@ -9,3 +9,33 @@ type Logistic struct {
 	IsFree               bool    `json:"is_free"`
 	EstimatedShippingFee float64 `json:"estimated_shipping_fee"`
 }
+
+type LogisticService interface {
+	Init(uint64, string) error
+}
+
+// LogisticServiceOp handles communication with the logistics related methods of
+// the Shopee API.
+type LogisticServiceOp struct {
+	client *Client
+}
+
+type LogisticInitResponse struct {
+	TrackingNumber string `json:"tracking_number"`
+	RequestID      string `json:"request_id"`
+}
+
+// Init https://open.shopee.com/documents?module=3&type=1&id=389
+func (s *LogisticServiceOp) Init(sid uint64, ordersn, trackingNo string) error {
+	path := "/logistics/init"
+	wrappedData := map[string]interface{}{
+		"ordersn": ordersn,
+		"shopid":  sid,
+		"non_integrated": map[string]interface{}{
+			"tracking_no": trackingNo,
+		},
+	}
+	resource := new(LogisticInitResponse)
+	err := s.client.Post(path, wrappedData, resource)
+	return err
+}
